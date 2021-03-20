25339dcef9de93df6e684aeaa4c0dc927b6a2d1a
zcammack <zack.cammack@gmail.com>
2021-03-20 14:28:00 -0500

    Created basic controllers for both models. Most work will be done using movie controller.

---
+++ backend/critic-crack-api/app/controllers/category_controller.rb
@@ -0,0 +1,14 @@
+class CategoryController < ApplicationController
+
+    def index
+        @categories = Category.all
+        
+        render json: @categories
+    end
+
+    private
+    
+    def category_params
+        params.require(:category).permit(:title, :movie_id)
+    end
+end
---
+++ backend/critic-crack-api/app/controllers/movie_controller.rb
@@ -0,0 +1,45 @@
+class MovieController < ApplicationController
+    before_action :set_movie, only: [:show, :update, :destroy]
+
+    def index
+        @movies = Movie.all
+
+        render json: @movies
+    end
+
+    def show
+        render json: @movie
+    end
+
+    def create
+        @movie = Movie.new(movie_params)
+
+        if @movie.save
+            render json: @movie, status: :created, location: @movie
+        else
+            render json: @movie.errors, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        if @movie.update(movie_params)
+            render json: @movie
+        else
+            render json: @movie.errors, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @movie.destroy
+    end
+
+    private
+
+    def set_movie
+        @movie = Movie.find(params[:id])
+    end
+
+    def movie_params
+        params.require(:movie).permit(:name, :rating, :director, :plot, :category_id, categories_attributes: [:title])
+    end
+end
